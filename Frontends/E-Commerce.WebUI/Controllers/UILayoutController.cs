36cb32d100d5d0a5750fc0b91f764ce7ded49807
Talha <99talhayildiz@gmail.com>
2024-12-06 02:57:34 +0300

    Create the User Layout

---
+++ Frontends/E-Commerce.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult _UILayout()
+        {
+            return View();
+        }
+    }
+}
