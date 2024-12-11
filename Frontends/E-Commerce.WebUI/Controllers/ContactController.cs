2fb32d08c526e25721de588f1c0eac849a957153
Talha <99talhayildiz@gmail.com>
2024-12-12 02:08:50 +0300

    Completed the Web UI Preparation

---
+++ Frontends/E-Commerce.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/E-Commerce.WebUI/ViewComponents/ContactViewComponents/_ContactDetailComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.ViewComponents.ContactViewComponents
+{
+    public class _ContactDetailComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
