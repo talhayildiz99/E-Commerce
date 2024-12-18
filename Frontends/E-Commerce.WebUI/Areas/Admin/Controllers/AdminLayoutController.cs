7de40768386e16ca301b5afc90df489acccb975b
Talha <99talhayildiz@gmail.com>
2024-12-19 01:07:27 +0300

    Created the Admin Web Page

---
+++ Frontends/E-Commerce.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
