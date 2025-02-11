956bf28d28e5b399789dd13e4e871495c0aeee6f
Talha <99talhayildiz@gmail.com>
2025-02-12 01:58:19 +0300

    Completed the Comment Services Operations

--- Frontends/E-Commerce.WebUI/Controllers/ProductListController.cs
+++ Frontends/E-Commerce.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce.DtoLayer.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace E_Commerce.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index(string id)
         {
             ViewBag.i = id;
@@ -13,6 +22,31 @@ namespace E_Commerce.WebUI.Controllers
         public IActionResult ProductDetail(string id)
         {
             ViewBag.x = id;
+            return View();
+        }
+
+        [HttpGet]
+        public  PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ImageUrl = "Test";
+            createCommentDto.Rating = 1;
+            createCommentDto.CreatedDate =DateTime.Parse(DateTime.Now.ToShortDateString());
+            createCommentDto.Status = false;
+            createCommentDto.ProductId = "673a84369e5e82a4613bd48b";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCommentDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7021/api/Comments", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
