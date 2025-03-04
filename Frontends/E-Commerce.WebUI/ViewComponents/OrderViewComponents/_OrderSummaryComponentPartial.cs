c75f5e27a7de4ca2b2d7cbb760038dff427c0ff7
Talha <99talhayildiz@gmail.com>
2025-03-04 18:39:40 +0300

    Completed the Order Operations

--- Frontends/E-Commerce.WebUI/ViewComponents/OrderViewComponents/_OrderSummaryComponentPartial.cs
+++ Frontends/E-Commerce.WebUI/ViewComponents/OrderViewComponents/_OrderSummaryComponentPartial.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce.WebUI.Services.BasketServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce.WebUI.ViewComponents.OrderViewComponents
 {
     public class _OrderSummaryComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IBasketService _basketService;
+        public _OrderSummaryComponentPartial(IBasketService basketService)
         {
-            return View();
+            _basketService = basketService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var basketTotal = await _basketService.GetBasket();
+            var basketItems = basketTotal.BasketItems;
+            return View(basketItems);
         }
     }
 }
