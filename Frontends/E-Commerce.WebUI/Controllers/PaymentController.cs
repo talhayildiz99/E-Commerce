6ef4f19d0dcaefe15b4000ec26065cd09397214a
Talha <99talhayildiz@gmail.com>
2025-03-06 02:42:29 +0300

    Completed the Credit Card Script

---
+++ Frontends/E-Commerce.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "Ödeme Ekranı";
+            ViewBag.directory2 = "Kart İle Ödeme";
+            return View();
+        }
+    }
+}
