408878a9dbbad572e5041d8d52712ae59802e9ba
Talha <99talhayildiz@gmail.com>
2025-03-06 18:18:49 +0300

    User Area Operations

---
+++ Frontends/E-Commerce.WebUI/Areas/User/Controllers/CargoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.Areas.User.Controllers
+{
+    public class CargoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/E-Commerce.WebUI/Areas/User/Controllers/MyOrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.Areas.User.Controllers
+{
+    [Area("User")]
+    public class MyOrderController : Controller
+    {
+        public IActionResult MyOrderList()
+        {
+            return View();
+        }
+    }
+}
