f0bb6b773c751c31094be872b70a1691fbc971ba
Talha <99talhayildiz@gmail.com>
2025-03-11 20:45:13 +0300

    Completed the Error Page

---
+++ Frontends/E-Commerce.WebUI/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.WebUI.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+            return View();
+        }
+    }
+}
--- Frontends/E-Commerce.WebUI/Program.cs
+++ Frontends/E-Commerce.WebUI/Program.cs
@@ -159,6 +159,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404", "?code={0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
