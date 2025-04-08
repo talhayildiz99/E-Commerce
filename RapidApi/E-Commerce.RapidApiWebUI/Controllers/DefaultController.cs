8993e97632542f3f4dab98e441b096e9adaca669
Talha <99talhayildiz@gmail.com>
2025-04-09 02:24:40 +0300

    Completed the Rapid Api

---
+++ RapidApi/E-Commerce.RapidApiWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,55 @@
+﻿using E_Commerce.RapidApiWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace E_Commerce.RapidApiWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task<IActionResult> WeatherDetail()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/istanbul"),
+                Headers =
+    {
+        { "x-rapidapi-key", "598242eb3dmshf21e0b46f346c29p12cc32jsn7b333aa8a8a4" },
+        { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<WeatherViewModel.Rootobject>(body);
+                ViewBag.cityTemp = values.data.temp;
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> ExchangeRate()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&to_symbol=try&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "598242eb3dmshf21e0b46f346c29p12cc32jsn7b333aa8a8a4" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject>(body);
+                ViewBag.excRate = values.data.exchange_rate;
+                return View();
+            }
+        }
+    }
+}
